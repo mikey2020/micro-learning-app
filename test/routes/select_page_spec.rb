99a764e704aae07c1e2e7e96c355754b0a64d902
Andela Developer <superdafe@gmail.com>
2018-07-09 00:31:32 +0100

    chore(tests) write tests: - write model tests - write route/endpoint tests

--- test/routes/select_page_spec.rb
+++ test/routes/select_page_spec.rb
@@ -3,20 +3,27 @@ require_relative '../../app.rb'
 
 RSpec.describe App  do
 
-  # it 'should add user user to the database' do
-  #   post '/user/signup', params={ user: { username: "naruto", email: "naruto@email.com", password: "tester123" } }
-    
-  #   expect(last_response).to be_ok
-  # end
+  before(:context) do
+    UserCategory.create({ user_id: 1, category_id: 1})
+    UserCategory.create({ user_id: 1, category_id: 3})
+  end
 
-  # it 'should display select categories page with all categories' do
-  #   get '/categories'
+  it 'should display select categories page with all categories' do
+    get '/categories', {}, { 'rack.session' => { user_id: 1 } }
 
-  #   puts last_response
-  #   expect(last_response.body).to include("general")
-  #   expect(last_response.body).to include("technology")
-  #   expect(last_response.body).to include("sports")
-  #   expect(last_response.body).to include("business")
-  #   expect(last_response.body).to include("science")
-  # end
+    expect(last_response).to be_redirect
+    follow_redirect!
+
+    puts last_response.body
+
+
+     
+    expect(last_response.body).to include("Customize your learning")
+    expect(last_response.body).to include("general")
+    expect(last_response.body).to include("technology")
+    expect(last_response.body).to include("sports")
+    expect(last_response.body).to include("business")
+    expect(last_response.body).to include("science")
+  end
+
 end
